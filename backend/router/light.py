767ad3a0f9519e2326d04d9a48c8330ddd86312d
namririnz <nathmcfc31115@gmail.com>
2023-02-08 12:05:38 +0700

    Complete get_light_by_id & get_all_light

--- backend/router/light.py
+++ backend/router/light.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 from typing import List, Optional, Union
-from fastapi import APIRouter, Body, HTTPException
-from pydantic import BaseModel
 
 from config.database import db
+from fastapi import APIRouter, Body, HTTPException
+from pydantic import BaseModel
 
 
 class Light(BaseModel):
@@ -16,3 +16,25 @@ class Light(BaseModel):
 router = APIRouter(prefix="/light",
                    tags=["light"])
 
+@router.get("/")
+def get_all_light():
+    collection = db["smarthome_light"]
+    data = collection.find({}, {"_id": False})
+    result = list(data)
+
+    return {"data": result}
+
+
+@router.get("/{room_id}")
+def get_light(room_id: int):
+    collection = db["smarthome_light"]
+
+    values = collection.find({"room_id": room_id}, {"_id": False})
+
+    values = list(values)
+
+    if (len(values) == 0):
+        raise HTTPException(status_code=404, detail="Room not found")
+
+    result = values[0]
+    return {"data": result}
