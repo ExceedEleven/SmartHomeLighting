cd11a40064d5c8a78569033d66b62b25407c86c1
Ioarsh <banks4247@gmail.com>
2023-02-08 18:58:43 +0700

    add auto

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import FormControlLabel from '@mui/material/FormControlLabel';
-import { FormGroup } from '@mui/material';
 import Checkswitch from './component/Checkswitch';
+import AutoSwitch from './component/Autoswitch';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,14 +15,17 @@ function App() {
           <div className="room">
             <p>Room 1</p>
             <FormControlLabel control={<Checkswitch room_id={0} className='Switch'/>}/>
+            <FormControlLabel control={<AutoSwitch room_id={0} className='Switch'/>}/>
           </div>
           <div className="room">
             <p>Room 2</p>
             <FormControlLabel control={<Checkswitch room_id={1}/>}/>
+            <FormControlLabel control={<AutoSwitch room_id={1} className='Switch'/>}/>
           </div>
           <div className="room">
             <p>Room 3</p>
             <FormControlLabel control={<Checkswitch room_id={2}/>}/>
+            <FormControlLabel control={<AutoSwitch room_id={2} className='Switch'/>}/>
           </div>        
         </div>
     </div>
